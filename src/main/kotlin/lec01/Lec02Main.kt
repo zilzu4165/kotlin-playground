3a7532e90e72a3bc205c5506fd3f475730170931
zilzu <zilzu4165@naver.com>
2022-11-08 21:44:39 +0900

    코틀린에서 null을 다루는 방법 - 2

--- src/main/kotlin/lec01/Lec02Main.kt
+++ src/main/kotlin/lec01/Lec02Main.kt
@@ -4,29 +4,29 @@ package lec01
  *  코틀린에서 null을 다루는 방법
  */
 fun main() {
-    val str: String? = "abc"
+    val str: String? = null
     println(str?.length) // safe-call : null이 아닐때 동작하고 null이면 전체결과가 null
     println(str?.length ?: 0) // elvis-연산자 : 앞의 연산 결과가 null이면 뒤의 값을 사용
+
+    println(startsWithA1(str))
+    println(startsWithA3(str))
+//    println(startsWithA4(str))
+//    println(startsWithA4(str))
 }
 
 fun startsWithA1(str: String?): Boolean {
-
-    if (str == null) {
-        throw IllegalArgumentException("null이 들어왔습니다.")
-    }
-    return str.startsWith("A")
+    return str?.startsWith("A")
+        ?: throw IllegalArgumentException("null이 들어있습니다")
 }
 
 fun startsWithA2(str: String?): Boolean? {
-    if (str == null) {
-        return null
-    }
-    return str.startsWith("A")
+    return str?.startsWith("A")
 }
 
 fun startsWithA3(str: String?): Boolean {
-    if (str == null) {
-        return false;
-    }
-    return str.startsWith("A")
+    return str?.startsWith("A") ?: false
+}
+
+fun startsWithA4(str: String?): Boolean {
+    return str!!.startsWith("A")  // !! : 얘는 절대 null이 아니라는 표현, 컴파일에러 없이 정상 동작한다.
 }
