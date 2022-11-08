78b87366deef6ffe63ebf55be7d8fa61081c33aa
zilzu <zilzu4165@naver.com>
2022-11-08 22:25:18 +0900

    코틀린에서 type을 다루는 방법 - 1

---
+++ src/main/kotlin/lec01/Lec03Main.kt
@@ -0,0 +1,17 @@
+package lec01
+
+class Lec03Main {
+}
+
+/**
+ * 코틀린에서 Type을 다루는 방법
+ */
+
+fun main() {
+    // 코틀린에서는 선언된 기본값을 보고 타입을 추론한다.
+    val number1 = 3 // Int
+    val number2 = 3L // Long
+    val number3 = 3.0f // Float
+    val number4 = 3.0 // Double
+
+}
