c8ecae693140d9892e86397ba0ab74cf7e5f1e5a
zilzu <zilzu4165@naver.com>
2022-11-08 22:28:04 +0900

    코틀린에서 type을 다루는 방법 - 2

--- src/main/kotlin/lec01/Lec03Main.kt
+++ src/main/kotlin/lec01/Lec03Main.kt
@@ -14,4 +14,8 @@ fun main() {
     val number3 = 3.0f // Float
     val number4 = 3.0 // Double
 
+    val number5: Long = number1.toLong()  // 코틀린에서는 암시적 타입변경이 불가능하기때문에 toLong()
+
+    println(number1 + number2)
+
 }
