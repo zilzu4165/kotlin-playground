98d3ed96e01eba90ffb053fb810ec8411af4f0cb
zilzu <zilzu4165@naver.com>
2022-11-09 21:20:01 +0900

    코틀린에서 type을 다루는 방법 - 6 문자열 """ """

--- src/main/kotlin/lec01/Lec03Main.kt
+++ src/main/kotlin/lec01/Lec03Main.kt
@@ -8,14 +8,12 @@ class Lec03Main {
  */
 
 fun main() {
-    val person = Person("박성수", 100)
-    val age = 29
-    val height = 178
+    val name = "박성수"
 
-    println("이름 : ${person.name}, 나이 : $age, 키 : ${height}")
-//     변수 이름만 사용하더라도 ${변수}를 사용하는 것이
-//     1) 가독성
-//     2) 일괄 변환
-//     3) 정규식 활용
-//     측면에서 좋다.
+    val str = """
+        ABC
+        EFG
+        ${name}
+    """.trimIndent()
+    println(str)
 }
