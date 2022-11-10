dfaa64f56eaa5d781a46a6692b2f363f047ae64a
zilzu <zilzu4165@naver.com>
2022-11-10 19:37:35 +0900

    kotlin 조건문을 다루는 방법 - 3 when 역시 하나의 expression 이기 때문에 하나의 값으로 취급된다.

--- src/main/kotlin/lec01/Lec05Main.kt
+++ src/main/kotlin/lec01/Lec05Main.kt
@@ -3,38 +3,11 @@ package lec01
 class Lec05Main {
 }
 
-fun main() {
-
-}
-
-fun validateScoreIsNotNegative(score: Int) {
-    if (score !in 0..100) {
-        throw IllegalArgumentException("${score}는 0보다 작을 수 없습니다.")
-    }
-}
-fun validateScore(score: Int) {
-    if (score in 0..100) {  // 0 이상 100 이하
-
-    }
-}
-
-
-fun getPassOrFail(score: Int): String {
-    return if (score >= 50) { // kotlin 에서는 if-else 문이 하나의 expression, 하나의 값으로 취급한다.
-        "P"
-    } else {
-        "F"
-    }
-}
-
-fun getGrade(score: Int): String {
-    return if (score >= 90) {
-        "A"
-    }else if (score >= 80) {
-        "B"
-    }else if (score >= 70) {
-        "C"
-    }else {
-        "D"
+fun getGradeWithSwitch(score: Int): String {
+    return when (score / 10) {  // when 역시 하나의 Expression 이기 때문에 하나의 값으로 취급된다.
+        9 -> "A"
+        8 -> "B"
+        7 -> "C"
+        else -> "D"
     }
 }
