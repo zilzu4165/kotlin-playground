48b6a9934c0eee793c9385cfdf6cddc3c6198c2c
zilzu <zilzu4165@naver.com>
2022-11-10 19:12:32 +0900

    kotlin 연산자를 다루는 방법 - 4 /**  *  in / !in  *  - 컬렉션이나 범위에 포함되어 있다, 포함되어 있지 않다.  *  a..b  *  - a부터 b까지의 범위 객체를 생성한다.  *  a[i]  *  - a에서 특정 index i 로 값을 가져온다.  *  a[i] = b  *  - a의 특정 index i에 b를 넣는다  */

--- src/main/kotlin/lec01/Lec04Main.kt
+++ src/main/kotlin/lec01/Lec04Main.kt
@@ -3,17 +3,17 @@ package lec01
 class Lec04Main {
 }
 
-fun main() {
-    if (fun1() || fun2()){
-        println("본문")
-    }
-}
+/**
+ *  in / !in
+ *  - 컬렉션이나 범위에 포함되어 있다, 포함되어 있지 않다.
+ *  a..b
+ *  - a부터 b까지의 범위 객체를 생성한다.
+ *  a[i]
+ *  - a에서 특정 index i 로 값을 가져온다.
+ *  a[i] = b
+ *  - a의 특정 index i에 b를 넣는다
+ */
 
-fun fun1(): Boolean {
-    println("fun 1")
-    return false;
-}
-fun fun2(): Boolean {
-    println("fun 2")
-    return true;
+fun main() {
+
 }
