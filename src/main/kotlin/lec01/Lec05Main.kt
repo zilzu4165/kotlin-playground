a10048c9ae5611881c8eedc9c09c868b923c5ea6
zilzu <zilzu4165@naver.com>
2022-11-10 19:46:17 +0900

    kotlin 조건문을 다루는 방법 - 5 코틀린의 when 은 조건문에 다양한 expression 식이 올 수 있다.

--- src/main/kotlin/lec01/Lec05Main.kt
+++ src/main/kotlin/lec01/Lec05Main.kt
@@ -4,13 +4,11 @@ class Lec05Main {
 }
 
 fun main() {
-    val number = "A"
-    println(startsWithA(number))
+    judgeNumber(1)
 }
-
-fun startsWithA(obj: Any): Boolean {
-    return when (obj) {
-        is String -> obj.startsWith("A")
-        else -> false
+fun judgeNumber(number: Int) {
+    when (number) {
+        1, 0, -1 -> println("어디서 많이 본 숫자입니다")
+        else -> print("-1, 0, 1이 아닙니다.")
     }
 }
