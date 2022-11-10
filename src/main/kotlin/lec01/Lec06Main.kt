a40acc99ec249c2b185021e9465b2a89eb418080
zilzu <zilzu4165@naver.com>
2022-11-10 20:05:02 +0900

    kotlin 반복문을 다루는 방법 - 1 for each 문에서 Java는 : Kotlin은 in 을 사용한다. 전통적인 for문에서 Kotlin은 등차수열과 in을 사용한다. 그 외 for 문 문법은 모두 동일하다. while문과 do while문은 동일하다.

---
+++ src/main/kotlin/lec01/Lec06Main.kt
@@ -0,0 +1,30 @@
+package lec01
+
+class Lec06Main {
+}
+
+fun main() {
+    // ListOf 리스트를 만들어줌
+    val numbers = listOf(1L, 2L, 3L)
+    for (number in numbers) {
+        println(number)
+    }
+
+    for (i in 1..3) {
+        println(i)
+    }
+
+    for (i in 3 downTo 1) {
+        println(i)
+    }
+
+    for (i in 1..5 step 2) {
+        println(i)
+    }
+
+    var i = 1
+    while (i <= 3) {
+        println("while :: $i")
+        i++
+    }
+}
