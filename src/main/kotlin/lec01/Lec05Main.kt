5989d6f505f76ea92a41bd9f4dd622f79cd8a073
zilzu <zilzu4165@naver.com>
2022-11-10 19:49:52 +0900

    kotlin 조건문을 다루는 방법 - 6 코틀린의 when 은 java 보다 엄청 유연하다.

--- src/main/kotlin/lec01/Lec05Main.kt
+++ src/main/kotlin/lec01/Lec05Main.kt
@@ -4,11 +4,12 @@ class Lec05Main {
 }
 
 fun main() {
-    judgeNumber(1)
+    judgeNumber(0)
 }
 fun judgeNumber(number: Int) {
-    when (number) {
-        1, 0, -1 -> println("어디서 많이 본 숫자입니다")
-        else -> print("-1, 0, 1이 아닙니다.")
+    when {
+        number == 0 -> println("주어진 숫자는 0입니다.")
+        number % 2 == 0 -> println("주어진 숫자는 짝수입니다.")
+        else -> println("주어지는 숫자는 홀수입니다.")
     }
 }
