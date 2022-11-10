01a8d4a59b3ef110838f244f271244cbace4817b
zilzu <zilzu4165@naver.com>
2022-11-10 19:44:00 +0900

    kotlin 조건문을 다루는 방법 - 4 코틀린의 when 은 조건문에 다양한 expression 식이 올 수 있다.

--- src/main/kotlin/lec01/Lec05Main.kt
+++ src/main/kotlin/lec01/Lec05Main.kt
@@ -3,12 +3,14 @@ package lec01
 class Lec05Main {
 }
 
-fun getGradeWithSwitch(score: Int): String {
-    // switch case -> when
-    return when (score) {  // when 역시 하나의 Expression 이기 때문에 하나의 값으로 취급된다.
-        in 90..99 -> "A"
-        in 80..89 -> "B"
-        in 70..79 -> "C"
-        else -> "D"
+fun main() {
+    val number = "A"
+    println(startsWithA(number))
+}
+
+fun startsWithA(obj: Any): Boolean {
+    return when (obj) {
+        is String -> obj.startsWith("A")
+        else -> false
     }
 }
