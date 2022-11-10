baf5e511d6c3a9be8b416c84b3c621f4b37d809c
zilzu <zilzu4165@naver.com>
2022-11-10 19:35:23 +0900

    kotlin 조건문을 다루는 방법 - 3 in, !in 0..100

--- src/main/kotlin/lec01/Lec05Main.kt
+++ src/main/kotlin/lec01/Lec05Main.kt
@@ -8,10 +8,16 @@ fun main() {
 }
 
 fun validateScoreIsNotNegative(score: Int) {
-    if (score < 0) {
+    if (score !in 0..100) {
         throw IllegalArgumentException("${score}는 0보다 작을 수 없습니다.")
     }
 }
+fun validateScore(score: Int) {
+    if (score in 0..100) {  // 0 이상 100 이하
+
+    }
+}
+
 
 fun getPassOrFail(score: Int): String {
     return if (score >= 50) { // kotlin 에서는 if-else 문이 하나의 expression, 하나의 값으로 취급한다.
