7dbb846c6cf2c2c86d776ff994e647efdaf6df78
zilzu <zilzu4165@naver.com>
2022-11-10 19:10:24 +0900

    kotlin 연산자를 다루는 방법 - 3 java와 동일하게 kotlin도 lazy연산을 수행한다. &&, ||, !

--- src/main/kotlin/lec01/Lec04Main.kt
+++ src/main/kotlin/lec01/Lec04Main.kt
@@ -4,10 +4,16 @@ class Lec04Main {
 }
 
 fun main() {
-    val money1 = JavaMoney(1_000L)
-    val money2 = money1
-    val money3 = JavaMoney(1_000L)
+    if (fun1() || fun2()){
+        println("본문")
+    }
+}
 
-    println(money1 == money3)  // == 비교 시 equals를 간접적으로 호출해준다.
-
+fun fun1(): Boolean {
+    println("fun 1")
+    return false;
+}
+fun fun2(): Boolean {
+    println("fun 2")
+    return true;
 }
