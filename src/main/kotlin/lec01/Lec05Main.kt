a10f13031b6ae1d37ca76d5ce5fd3f6f7535d085
zilzu <zilzu4165@naver.com>
2022-11-10 19:39:34 +0900

    kotlin 조건문을 다루는 방법 - 3 코틀린의 when 은 in .. 을 통해 하나의 JAVA의 case 처럼 하나의 값일때만 사용할수 있는게 아니라 범위를 지정해줄 수 있다.

--- src/main/kotlin/lec01/Lec05Main.kt
+++ src/main/kotlin/lec01/Lec05Main.kt
@@ -4,10 +4,11 @@ class Lec05Main {
 }
 
 fun getGradeWithSwitch(score: Int): String {
-    return when (score / 10) {  // when 역시 하나의 Expression 이기 때문에 하나의 값으로 취급된다.
-        9 -> "A"
-        8 -> "B"
-        7 -> "C"
+    // switch case -> when
+    return when (score) {  // when 역시 하나의 Expression 이기 때문에 하나의 값으로 취급된다.
+        in 90..99 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
         else -> "D"
     }
 }
