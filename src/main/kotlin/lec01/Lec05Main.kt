f8ba8f0c5a7fdef5202fd32cbde96ce983599d04
zilzu <zilzu4165@naver.com>
2022-11-10 19:32:58 +0900

    kotlin 조건문을 다루는 방법 - 2 kotlin 에서는 if-else 문을 하나의 값으로 사용될 수 있다.

--- src/main/kotlin/lec01/Lec05Main.kt
+++ src/main/kotlin/lec01/Lec05Main.kt
@@ -6,15 +6,29 @@ class Lec05Main {
 fun main() {
 
 }
+
 fun validateScoreIsNotNegative(score: Int) {
     if (score < 0) {
         throw IllegalArgumentException("${score}는 0보다 작을 수 없습니다.")
     }
 }
-fun getPassOrFail(score: Int):String {
+
+fun getPassOrFail(score: Int): String {
     return if (score >= 50) { // kotlin 에서는 if-else 문이 하나의 expression, 하나의 값으로 취급한다.
         "P"
     } else {
         "F"
+    }
+}
+
+fun getGrade(score: Int): String {
+    return if (score >= 90) {
+        "A"
+    }else if (score >= 80) {
+        "B"
+    }else if (score >= 70) {
+        "C"
+    }else {
+        "D"
     }
 }
