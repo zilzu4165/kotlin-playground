563a8edcfba62ec827032066298c4b07c8aeafb7
zilzu <zilzu4165@naver.com>
2022-11-13 19:31:32 +0900

    kotlin function - 1 default parameter

---
+++ src/main/kotlin/lec01/Lec08Main.kt
@@ -0,0 +1,24 @@
+package lec01
+
+class Lec08Main {
+}
+
+fun main() {
+    repeat("Hello World")
+}
+
+fun max(a: Int, b: Int) = if (a > b) a else b
+
+fun repeat(  // default parameter 밖에서 파라미터를 넣어주지 않으면 기본값을 사용한다.
+    str: String,
+    num: Int = 3,
+    useNewLine: Boolean = true
+) {
+    for (i in 1..num) {
+        if (useNewLine) {
+            println(str)
+        }else {
+            print(str)
+        }
+    }
+}
