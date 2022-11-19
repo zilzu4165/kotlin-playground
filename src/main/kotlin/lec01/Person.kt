78de29c00d854ab3da1d5782ead1df80e22383e0
zilzu <zilzu4165@naver.com>
2022-11-19 12:58:22 +0900

    kotlin class - 5 주 생성자, 부 생성자

--- src/main/kotlin/lec01/Person.kt
+++ src/main/kotlin/lec01/Person.kt
@@ -1,13 +1,10 @@
 package lec01
 
 fun main() {
-    val person = JavaPerson("박성수", 100)
-    println(person.name)
-    person.age = 200
-    println(person.age)
+    Person()
 }
 
-class Person(
+class Person(  // --> 주 생성자(primary constructor) : 반드시 존재해야함, 단, 파라미터가 하나도 없다면 없어도 무
     val name: String,
     var age: Int)
 {
@@ -15,6 +12,13 @@ class Person(
         if (age <= 0) {
             throw IllegalArgumentException("나이는 ${age}일 수 없습니다.")
         }
+        println("초기화 블록")
     }
-    constructor(name: String) : this(name, 1)
+    constructor(name: String) : this(name, 1){  // --> 부 생성자(secondary constructor) : this 는 다른 생성자를 가리킴
+        println("부 생성자 1")
+    }
+
+    constructor(): this("이수현") {
+        println("부 생성자 2")
+    }
 }
