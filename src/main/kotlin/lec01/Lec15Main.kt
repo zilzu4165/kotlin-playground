313be4e259c911bdf4b8525ea21b5201f6aec113
zilzu <zilzu4165@naver.com>
2022-11-22 22:13:39 +0900

    kotlin array-collection - 4 map

--- src/main/kotlin/lec01/Lec15Main.kt
+++ src/main/kotlin/lec01/Lec15Main.kt
@@ -4,15 +4,9 @@ class Lec15Main {
 }
 
 fun main() {
-    val numbers = setOf(100, 200) // 가변
-//    val numbers = mutableSetOf<>() Of(100, 200) // 불변
+    val oldMap = mutableMapOf<Int, String>()
+    oldMap[1] = "Monday"
+    oldMap[2] = "Tuesday"
 
-
-    for (number in numbers) {
-        println(number)
-    }
-
-    for ((idx, value) in numbers.withIndex()) {
-        println("${idx} ${value}")
-    }
+    val mapOf = mapOf(1 to "Monday", 2 to "Tuesday")
 }
