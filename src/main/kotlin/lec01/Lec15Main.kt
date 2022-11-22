de3987101541d899d6a24856ae8bb7abc6ab30c5
zilzu <zilzu4165@naver.com>
2022-11-22 22:10:56 +0900

    kotlin array-collection - 2 mutableList

--- src/main/kotlin/lec01/Lec15Main.kt
+++ src/main/kotlin/lec01/Lec15Main.kt
@@ -4,8 +4,17 @@ class Lec15Main {
 }
 
 fun main() {
-    val numbers = listOf(100, 200)
-    val emptyList = emptyList<Int>() // 비어있는 리스트는 추론을 못하기 때문에 무슨타입인지 넣어주어야함
+//    val numbers = listOf(100, 200) // 가변
+    val numbers = mutableListOf(100, 200) // 불변
 
 
+    println(numbers[0])
+
+    for (number in numbers) {
+        println(number)
+    }
+
+    for ((idx, value) in numbers.withIndex()) {
+        println("${idx} ${value}")
+    }
 }
