3990139088c9df90ecb1f346e042c9802ec536bf
zilzu <zilzu4165@naver.com>
2022-11-22 22:15:05 +0900

    kotlin array-collection - 4 map 활용

--- src/main/kotlin/lec01/Lec15Main.kt
+++ src/main/kotlin/lec01/Lec15Main.kt
@@ -8,5 +8,13 @@ fun main() {
     oldMap[1] = "Monday"
     oldMap[2] = "Tuesday"
 
-    val mapOf = mapOf(1 to "Monday", 2 to "Tuesday")
+    for (key in oldMap.keys) {
+        println(key)
+        println(oldMap[key])
+    }
+
+    for ((key, value) in oldMap.entries) {
+        println(key)
+        println(value)
+    }
 }
