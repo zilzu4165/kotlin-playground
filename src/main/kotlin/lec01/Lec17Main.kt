d27f02e09e88ec0fbd82e905fe187d44762712d2
zilzu <zilzu4165@naver.com>
2022-12-02 22:54:19 +0900

    kotlin lambda - 2     filterFruits(fruits, isApple)     filterFruits(fruits) { fruit: Fruit -> fruit.name == "사과" }     filterFruits(fruits) { it.name == "사과" }

--- src/main/kotlin/lec01/Lec17Main.kt
+++ src/main/kotlin/lec01/Lec17Main.kt
@@ -10,19 +10,29 @@ fun main() {
         Fruit("사과", 1_200),
         Fruit("사과", 1_500),
         Fruit("바나나", 3_000),
-        Fruit("바나나",3_200),
+        Fruit("바나나", 3_200),
         Fruit("바나나", 2_500),
         Fruit("수박", 10_000)
 
     )
+
     val isApple: (Fruit) -> Boolean = fun(fruit: Fruit): Boolean {  // 이름 없는 함수 --> 람다
         return fruit.name == "사과"
     }
 
     val isApple2: (Fruit) -> Boolean = { fruit: Fruit -> fruit.name == "사과" }  // -> 이용한 방법
 
-    isApple(fruits[0])
-    isApple.invoke(fruits[0]) // invoke : 부르다, 호출하다
-
-
+    filterFruits(fruits, isApple)
+    filterFruits(fruits) { fruit: Fruit -> fruit.name == "사과" }
+    filterFruits(fruits) { it.name == "사과" }
 }
+
+    private fun filterFruits(fruits: List<Fruit>, filter: (Fruit) -> Boolean): List<Fruit> {
+        val results = mutableListOf<Fruit>()
+        for (fruit in fruits) {
+            if (filter(fruit)) {
+                results.add(fruit)
+            }
+        }
+        return results
+    }
